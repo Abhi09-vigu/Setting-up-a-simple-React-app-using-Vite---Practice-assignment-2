6bd68a2625147222626c3c22ff350595874012c5
Abhi <abhivignesh27@gmail.com>
2025-03-05 14:33:41 +0530

    done

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Name:Abhi vignesh</h1>
+        <h2>Email:abhivignesh27@gmail.com</h2>
+        <h3>Phone:9701976318</h3>
+      </div> 
+    </>
+  )
+}
+
+export default App
